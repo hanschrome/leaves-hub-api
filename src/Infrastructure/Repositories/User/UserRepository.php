3e73583b6db7ea19c80a41356204877595b86cae
Hans <hanscastroadmn@gmail.com>
2022-07-20 16:36:07 +0000

    [Infra] Added UserRepository

---
+++ src/Infrastructure/Repositories/User/UserRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Infrastructure\Repositories\User;
+
+use Src\Domain\Repositories\IUserRepository;
+
+class UserRepository implements IUserRepository
+{
+
+}
