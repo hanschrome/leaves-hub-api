5c130e3716b6cb8ca0ed1e702bec3099c2ff35bf
Hans <hanscastroadmn@gmail.com>
2022-08-02 16:22:01 +0000

    [Domain] User

--- src/Domain/User/IUser.php
+++ src/Domain/User/IUser.php
@@ -5,15 +5,23 @@ declare(strict_types=1);
 namespace Src\Domain\User;
 
 use Src\Domain\User\Properties\IUserId;
+use Src\Domain\User\Properties\UserCreatedAt\IUserCreatedAt;
 use Src\Domain\User\Properties\UserEmail\IUserEmail;
 use Src\Domain\User\Properties\UserEmailVerifiedAt\IUserVerifiedAt;
+use Src\Domain\User\Properties\UserPassword\IUserPassword;
+use Src\Domain\User\Properties\UserUpdatedAt\IUserUpdatedAt;
+use Src\Domain\User\Properties\UserVerifyToken\IUserVerifyToken;
 
 interface IUser
 {
     public function __construct(
         IUserId $userId,
         IUserEmail $userEmail,
-        IUserVerifiedAt $emailVerifiedAt
+        IUserVerifiedAt $emailVerifiedAt,
+        IUserPassword $userPassword,
+        IUserVerifyToken $userVerifyToken,
+        IUserUpdatedAt $userUpdatedAt,
+        IUserCreatedAt $userCreatedAt
     );
 
     public function getId(): IUserId;
@@ -21,4 +29,12 @@ interface IUser
     public function getEmail(): IUserEmail;
 
     public function getEmailVerifiedAt(): IUserVerifiedAt;
+
+    public function getPassword(): IUserPassword;
+
+    public function getVerifyToken(): IUserVerifyToken;
+
+    public function getUpdatedAt(): IUserUpdatedAt;
+
+    public function getCreatedAt(): IUserCreatedAt;
 }
