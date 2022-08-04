a128f21a32ccc1bd89bc02919bb62d2bcb0af17d
Hans <hanscastroadmn@gmail.com>
2022-08-04 05:39:37 +0000

    [User session] Session id property implementation

---
+++ src/Domain/UserSession/Properties/UserSessionId/UserSessionId.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Domain\UserSession\Properties\UserSessionId;
+
+use Src\Domain\Properties\AbstractProperty;
+use Src\Domain\Properties\IProperty;
+use Src\Domain\UserSession\Properties\UserSessionId\Validator\UserSessionIdValidator;
+
+class UserSessionId extends AbstractProperty implements IProperty
+{
+    private string $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function sanitize(): void
+    {
+        $this->value = trim($this->value);
+    }
+
+    public function validate(): void
+    {
+        $this->addValidator(new UserSessionIdValidator($this));
+        $this->executeValidators();
+    }
+
+    public function value(): string
+    {
+        return $this->value;
+    }
+}
