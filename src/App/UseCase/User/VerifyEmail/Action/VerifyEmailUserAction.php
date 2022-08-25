9268f94d94a42cbd122e928aed5a60e36a100355
Hans <hanscastroadmn@gmail.com>
2022-08-25 17:09:50 +0000

    [User Verify Email] Verify email user action

---
+++ src/App/UseCase/User/VerifyEmail/Action/VerifyEmailUserAction.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\App\UseCase\User\VerifyEmail\Action;
+
+use Src\Domain\User\Properties\UserId;
+use Src\Domain\User\Properties\UserVerifyToken\UserVerifyToken;
+use Src\Domain\User\Services\VerifyEmail\IUserVerifyEmailService;
+use Src\Infrastructure\Api\Response\IResponse;
+use Src\Infrastructure\Api\User\Response\VerifyEmail\VerifyEmailUserResponse;
+
+class VerifyEmailUserAction
+{
+    private IUserVerifyEmailService $userVerifyEmailService;
+
+    public function __construct(IUserVerifyEmailService $userVerifyEmailService)
+    {
+        $this->userVerifyEmailService = $userVerifyEmailService;
+    }
+
+    public function __invoke(array $requestJsonBody): IResponse
+    {
+        $userUuid = $requestJsonBody['uuid'];
+        $verifyToken = $requestJsonBody['verifyToken'];
+
+        try {
+            $successful = $this->userVerifyEmailService->verifyUserByIdAndToken(new UserId($userUuid), new UserVerifyToken($verifyToken));
+        } catch (\Throwable $throwable) {
+            $successful = false;
+        }
+
+        return new VerifyEmailUserResponse(null, $successful);
+    }
+}
