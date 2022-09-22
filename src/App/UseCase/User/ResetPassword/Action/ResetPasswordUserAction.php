da2c04ee8c1a425bff6ce21c337d5d4d18ce9b01
Hans Castro <hanscastroadmn@gmail.com>
2022-09-22 20:01:27 +0200

    Added reset password user action

--- src/App/UseCase/User/ResetPassword/Action/ResetPasswordUserAction.php
+++ src/App/UseCase/User/ResetPassword/Action/ResetPasswordUserAction.php
@@ -4,26 +4,39 @@ declare(strict_types=1);
 
 namespace Src\App\UseCase\User\ResetPassword\Action;
 
+use Src\Domain\Repositories\IUserRepository;
 use Src\Domain\User\Properties\UserEmail\UserEmail;
 use Src\Domain\User\Services\ResetPassword\IUserResetPasswordService;
 use Src\Infrastructure\Api\Response\IResponse;
 use Src\Infrastructure\Api\User\Response\ResetPassword\ResetPasswordUserResponse;
+use Src\Infrastructure\Mailing\IMailingService;
 
 class ResetPasswordUserAction
 {
     private IUserResetPasswordService $userResetPasswordService;
+    private IMailingService $mailingService;
+    private IUserRepository $userRepository;
 
-    public function __construct(IUserResetPasswordService $userResetPasswordService)
+    public function __construct(
+        IUserResetPasswordService $userResetPasswordService,
+        IMailingService $mailingService,
+        IUserRepository $userRepository
+    )
     {
         $this->userResetPasswordService = $userResetPasswordService;
+        $this->mailingService = $mailingService;
+        $this->userRepository = $userRepository;
     }
 
     public function __invoke(array $requestJsonBody): IResponse
     {
-        $email = $requestJsonBody['email'];
+        $email = new UserEmail($requestJsonBody['email']);
 
-        $success = $this->userResetPasswordService->resetPasswordByEmail(new UserEmail($email));
+        $user = $this->userRepository->findUserByEmail($email);
 
-        return new ResetPasswordUserResponse(null, $success);
+        $this->userResetPasswordService->resetPasswordByEmail($user->getEmail()); // @todo throw exception if not possible
+        $this->mailingService->sendMail($user, '', ''); // @todo add the subject and body
+
+        return new ResetPasswordUserResponse(null, true);
     }
 }
