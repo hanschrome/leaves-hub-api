ed587a3206ca1d3bd93b7ffa7d08824d2855f1d8
Hans Castro <hanscastroadmn@gmail.com>
2022-10-05 07:22:07 +0200

    [User] User login

--- src/App/UseCase/User/Login/Action/LoginUserAction.php
+++ src/App/UseCase/User/Login/Action/LoginUserAction.php
@@ -7,6 +7,7 @@ namespace Src\App\UseCase\User\Login\Action;
 use Src\Domain\User\Properties\UserEmail\UserEmail;
 use Src\Domain\User\Properties\UserPassword\UserPassword;
 use Src\Domain\User\Services\Login\IUserLoginService;
+use Src\Domain\UserSession\Services\Login\IUserSessionLoginService;
 use Src\Infrastructure\Api\Response\IResponse;
 use Src\Infrastructure\Api\Response\Login\LoginSuccessUserResponse;
 use Src\Infrastructure\Api\Response\Login\LoginWrongUserResponse;
@@ -16,26 +17,31 @@ class LoginUserAction
     public const PARAM_EMAIL = 'email';
     public const PARAM_PASSWORD = 'password';
     private IUserLoginService $userLoginService;
+    private IUserSessionLoginService $userSessionLoginService;
 
-    public function __construct(IUserLoginService $userLoginService)
+    public function __construct(
+        IUserLoginService $userLoginService,
+        IUserSessionLoginService $userSessionLoginService
+    )
     {
         $this->userLoginService = $userLoginService;
+        $this->userSessionLoginService = $userSessionLoginService;
     }
 
     public function __invoke(array $requestJsonBody): IResponse
     {
         $email = $requestJsonBody[self::PARAM_EMAIL];
         $password = $requestJsonBody[self::PARAM_PASSWORD];
-
-        $success = true;
+        $userSession = null;
 
         try {
             $user = $this->userLoginService->startSessionByEmailAndPassword(new UserEmail($email), new UserPassword($password));
-            // @todo add here the session
+
+            $userSession = $this->userSessionLoginService->startSessionByUser($user);
         } catch (\Throwable $throwable) { // @todo log and handle better this exception
-            $success = false;
+
         }
 
-        return $success ? new LoginSuccessUserResponse() : new LoginWrongUserResponse();
+        return $userSession ? new LoginSuccessUserResponse($userSession->getId()) : new LoginWrongUserResponse();
     }
 }
--- src/Infrastructure/Api/User/Response/Login/LoginSuccessUserResponse.php
+++ src/Infrastructure/Api/User/Response/Login/LoginSuccessUserResponse.php
@@ -4,14 +4,22 @@ declare(strict_types=1);
 
 namespace Src\Infrastructure\Api\Response\Login;
 
+use Src\Domain\UserSession\Properties\UserSessionId\IUserSessionId;
 use Src\Infrastructure\Api\Response\IResponse;
 
 class LoginSuccessUserResponse implements IResponse
 {
+    private IUserSessionId $userSessionId;
+
+    public function __construct(IUserSessionId $userSessionId)
+    {
+        $this->userSessionId = $userSessionId;
+    }
+
     function toArray(): array
     {
         return [
-            'session_id' => ''
+            'session_id' => $this->userSessionId->value()
         ];
     }
 }
