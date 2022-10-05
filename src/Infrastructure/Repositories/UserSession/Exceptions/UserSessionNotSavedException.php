3c3a573eaa662350df8e030858f4603d6c5a8501
Hans Castro <hanscastroadmn@gmail.com>
2022-10-05 07:17:23 +0200

    [User] User session repository

---
+++ src/Infrastructure/Repositories/UserSession/Exceptions/UserSessionNotSavedException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Infrastructure\Repositories\UserSession\Exceptions;
+
+use Exception;
+
+class UserSessionNotSavedException extends Exception
+{
+
+}
--- src/Infrastructure/Repositories/UserSession/UserSessionEloquentRepository.php
+++ src/Infrastructure/Repositories/UserSession/UserSessionEloquentRepository.php
@@ -4,14 +4,31 @@ declare(strict_types=1);
 
 namespace Src\Infrastructure\Repositories\UserSession;
 
+use App\Models\UserSessionEloquentModel;
 use Src\Domain\UserSession\IUserSession;
 use Src\Domain\UserSession\Repositories\IUserSessionRepository;
+use Src\Infrastructure\Repositories\UserSession\Exceptions\UserSessionNotSavedException;
 
 class UserSessionEloquentRepository implements IUserSessionRepository
 {
 
+    /**
+     * @throws UserSessionNotSavedException
+     */
     public function create(IUserSession $userSession): IUserSession
     {
-        // TODO: Implement createSessionByUser() method.
+        $userSessionEloquentModel = new UserSessionEloquentModel();
+
+        $userSessionEloquentModel->uuid = $userSession->getId()->value();
+        $userSessionEloquentModel->user_uuid = $userSession->getUserId()->value();
+        $userSessionEloquentModel->expiration_date = $userSession->getExpirationDate()->value();
+        $userSessionEloquentModel->updated_at = $userSession->getUpdatedAt()->value();
+        $userSessionEloquentModel->created_at = $userSession->getCreatedAt()->value();
+
+        if (!$userSessionEloquentModel->save()) { // @todo log in Sentry
+            throw new UserSessionNotSavedException('Error in save');
+        }
+
+        return $userSession;
     }
 }
