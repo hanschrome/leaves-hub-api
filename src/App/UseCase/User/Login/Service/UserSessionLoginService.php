7c017e817730c080c94049093cd359cd6421f519
Hans Castro <hanscastroadmn@gmail.com>
2022-10-05 07:10:03 +0200

    [User] User login

--- src/App/UseCase/User/Login/Service/UserSessionLoginService.php
+++ src/App/UseCase/User/Login/Service/UserSessionLoginService.php
@@ -4,10 +4,20 @@ declare(strict_types=1);
 
 namespace Src\App\UseCase\User\Login\Service;
 
+use Ramsey\Uuid\Uuid;
+use Src\App\UseCase\User\Login\Exception\LoginUserActionUserWrongStatusException;
 use Src\Domain\User\IUser;
+use Src\Domain\User\Properties\UserStatus\UserStatus;
 use Src\Domain\UserSession\IUserSession;
+use Src\Domain\UserSession\Properties\UserSessionCreatedAt\UserSessionCreatedAt;
+use Src\Domain\USerSession\Properties\UserSessionExpirationDate\UserSessionExpirationDate;
+use Src\Domain\UserSession\Properties\UserSessionId\UserSessionId;
+use Src\Domain\UserSession\Properties\UserSessionStatus\IUserSessionStatus;
+use Src\Domain\UserSession\Properties\UserSessionStatus\UserSessionStatus;
+use Src\Domain\UserSession\Properties\UserSessionUpdatedAt\UserSessionUpdatedAt;
 use Src\Domain\UserSession\Repositories\IUserSessionRepository;
 use Src\Domain\UserSession\Services\Login\IUserSessionLoginService;
+use Src\Domain\UserSession\UserSession;
 
 class UserSessionLoginService implements IUserSessionLoginService
 {
@@ -18,8 +28,23 @@ class UserSessionLoginService implements IUserSessionLoginService
         $this->userSessionRepository = $userSessionRepository;
     }
 
+    /**
+     * @throws LoginUserActionUserWrongStatusException
+     */
     public function startSessionByUser(IUser $user): IUserSession
     {
-        // @todo
+        if ($user->getStatus()->value() !== UserStatus::ACTIVE) {
+            throw new LoginUserActionUserWrongStatusException();
+        }
+
+        $userSession = new UserSession(
+            new UserSessionId(Uuid::uuid4()->toString()),
+            new UserSessionStatus(IUserSessionStatus::STATUS_VALID),
+            new UserSessionExpirationDate(null),
+            new UserSessionUpdatedAt(now()->timestamp),
+            new UserSessionCreatedAt(now()->timestamp)
+        );
+
+        return $this->userSessionRepository->create($userSession);
     }
 }
