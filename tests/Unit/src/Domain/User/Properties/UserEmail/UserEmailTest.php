c313869eb563e7e8af8684dfb1b002284069aad0
Hans Castro <hanscastroadmn@gmail.com>
2022-12-21 19:19:24 +0100

    [Tests] Unit tests for UserEmailTest.php

---
+++ tests/Unit/src/Domain/User/Properties/UserEmail/UserEmailTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\src\Domain\User\Properties\UserEmail;
+
+use Src\Domain\Properties\Validators\Exceptions\IPropertyException;
+use Src\Domain\User\Properties\UserEmail\UserEmail;
+use Tests\TestCase;
+
+class UserEmailTest extends TestCase
+{
+    /**
+     * @dataProvider Tests\Unit\src\Domain\User\Properties\UserEmail\UserEmailDataProviderTest::test_value_without_exception
+     */
+    public function test_values(string $value): void
+    {
+        $sut = new UserEmail($value);
+
+        $this->assertEquals($value, $sut->value());
+    }
+
+    /**
+     * @dataProvider Tests\Unit\src\Domain\User\Properties\UserEmail\UserEmailDataProviderTest::test_value_with_exception
+     */
+    public function test_wrong_values(string $value): void
+    {
+        $this->expectException(IPropertyException::class);
+
+        new UserEmail($value);
+    }
+}
