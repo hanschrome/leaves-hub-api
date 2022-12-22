68314c0820e894c02dfd8e0867b1554a25ecd63c
Hans Castro <hanscastroadmn@gmail.com>
2022-12-22 08:18:14 +0100

    [Tests] Unit tests for UserEmailRegexPropertyValidatorTest.php

---
+++ tests/Unit/src/Domain/User/Properties/UserVerifiedAt/UserEmailVerifiedAtTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\src\Domain\User\Properties\UserVerifiedAt;
+
+use Src\Domain\User\Properties\UserEmailVerifiedAt\UserEmailVerifiedAt;
+use Src\Domain\User\Properties\UserEmailVerifiedAt\Validators\UserVerifiedAtTimestampPropertyValidator\UserVerifiedAtTimestampPropertyWrongRangeException;
+use Tests\TestCase;
+
+class UserEmailVerifiedAtTest extends TestCase
+{
+    public function test_valid(): void
+    {
+        $sut = new UserEmailVerifiedAt(now()->timestamp);
+
+        $sut->validate();
+
+        $this->assertTrue(true);
+    }
+
+    public function test_invalid(): void
+    {
+        $this->expectException(UserVerifiedAtTimestampPropertyWrongRangeException::class);
+
+        $sut = new UserEmailVerifiedAt(-1);
+
+        $sut->validate();
+    }
+}
---
+++ tests/Unit/src/Domain/User/Properties/UserVerifiedAt/Validators/UserVerifiedAtTimestampPropertyValidatorTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\src\Domain\User\Properties\UserVerifiedAt\Validators;
+
+use Src\Domain\User\Properties\UserEmailVerifiedAt\Validators\UserVerifiedAtTimestampPropertyValidator\UserVerifiedAtTimestampPropertyValidator;
+use Src\Domain\User\Properties\UserEmailVerifiedAt\Validators\UserVerifiedAtTimestampPropertyValidator\UserVerifiedAtTimestampPropertyWrongRangeException;
+use Tests\TestCase;
+use Tests\Unit\src\Domain\Properties\AbstractTimestamp\Validators\TimestampPropertyRangeValidator\DummyIProperty;
+
+class UserVerifiedAtTimestampPropertyValidatorTest extends TestCase
+{
+
+    /**
+     * @throws UserVerifiedAtTimestampPropertyWrongRangeException
+     */
+    public function test_valid(): void
+    {
+        $sut = new UserVerifiedAtTimestampPropertyValidator(new DummyIProperty(now()->timestamp));
+
+        $sut->validate();
+
+        $this->assertTrue(true);
+    }
+
+    public function test_invalid(): void
+    {
+        $this->expectException(UserVerifiedAtTimestampPropertyWrongRangeException::class);
+
+        $sut = new UserVerifiedAtTimestampPropertyValidator(new DummyIProperty(-1));
+
+        $sut->validate();
+    }
+}
